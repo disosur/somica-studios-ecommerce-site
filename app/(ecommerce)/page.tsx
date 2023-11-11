204008e90fc85e991553e25ac7efcc20b4a76a8b
Alyssair Renhee Nikolai Morales <alymond.business@outlook.com>
2023-11-11 17:57:30 +0800

    Laid out layout

--- app/(ecommerce)/page.tsx
+++ app/(ecommerce)/page.tsx
@@ -31,9 +31,59 @@ export default function Home() {
       </section>
 
       {/*offerings section*/}
-      <section>
-        <div className="container flex flex-col items-center justify-center px-5 mx-auto">
+      <section className="">
+        <div className="container grid items-center justify-center grid-flow-col grid-cols-2 grid-rows-2 gap-4 px-5 mx-auto">
           <LinkCard />
+          <LinkCard />
+          <LinkCard />
+          <LinkCard />
+        </div>
+      </section>
+
+      <section>
+        <LinkCard />
+      </section>
+
+      {/*Best Sellers section*/}
+      <section></section>
+
+      {/*Our Story section*/}
+      <section></section>
+
+      {/*Featured section*/}
+      <section></section>
+
+      {/*offering and newsletter section*/}
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col w-full mb-12 text-center">
+            <h1 className="mb-4 text-2xl font-medium text-gray-900 sm:text-3xl title-font">
+              Master Cleanse Reliac Heirloom
+            </h1>
+            <p className="mx-auto text-base leading-relaxed lg:w-2/3">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              haven't heard of them man bun deep.
+            </p>
+          </div>
+          <div className="flex flex-col items-end w-full px-8 mx-auto space-y-4 lg:w-2/3 sm:flex-row sm:space-x-4 sm:space-y-0 sm:px-0">
+            <div className="relative flex-grow w-full">
+              <label
+                htmlFor="email"
+                className="text-sm leading-7 text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full px-3 py-1 text-base leading-8 text-gray-700 transition-colors duration-200 ease-in-out bg-gray-100 bg-opacity-50 border border-gray-300 rounded outline-none focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200"
+              />
+            </div>
+            <button className="px-8 py-2 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600">
+              Button
+            </button>
+          </div>
         </div>
       </section>
     </main>
