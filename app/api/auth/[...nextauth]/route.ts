1d9b54c2c92c1d8b5a5cc181375a7d28a403a487
Alyssair Renhee Nikolai Morales <alymond.business@outlook.com>
2023-11-27 18:46:21 +0800

    will this work

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,27 @@
-import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
-import { prisma } from "@/lib/db/prisma";
-import { env } from "@/lib/env";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
 import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db/prisma";
 import { Adapter } from "next-auth/adapters";
-import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth/next";
+import { mergeAnonymousCartIntoUserCart } from "@/lib/db/cart";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+
   callbacks: {
     session({ session, user }) {
       session.user.id = user.id;
       return session;
     },
   },
+
   events: {
     async signIn({ user }) {
       await mergeAnonymousCartIntoUserCart(user.id);
